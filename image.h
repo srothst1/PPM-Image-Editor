ecb9fd5d2766401420977a3558b6365dfc4802c2
srothst1 <39531367+srothst1@users.noreply.github.com>
2019-01-05 14:57:32 -0800

    Create image.h

---
+++ image.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+
+void noRed(int width, int height, int* ppm);
+void noGreen(int width, int height, int* ppm);
+void noBlue(int width, int height, int* ppm);
+void invert(int width, int height, int* ppm);
+void greyscale(int width, int height, int* ppm);
+void flipHorizontally(int width, int height, int* ppm);
+void flipVertically(int width, int height, int* ppm);
+void mirror(int width, int height, int* ppm);
+void simplify(int width, int height, int* ppm);
